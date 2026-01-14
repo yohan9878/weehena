e8902c1156a82b90cac33f06dd8edaedef50d297
Yohan Office <yohanoffice78@gmail.com>
2026-01-14 16:16:31 +0530

    Added animation for hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import SiteCard from "@/components/SiteCard";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,25 +10,46 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex flex-col justify-center md:mx-auto bg-center bg-black/50 min-w-screen min-h-screen bg-cover bg-blend-darken" style={{ backgroundImage: "url('/assets/company2.jpg')" }}>
-      <div>
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 2, ease: "easeOut" }}
+      className="flex flex-col justify-center md:mx-auto bg-center bg-black/50 min-w-screen min-h-screen bg-cover bg-blend-darken"
+      style={{ backgroundImage: "url('/assets/company2.jpg')" }}
+    >
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 2,
+          scale: { type: "spring", visualDuration: 2, bounce: 0.5 },
+        }}
+      >
         <Image
           src="/assets/weehenaLogo.png"
           alt="Company Logo"
-          width={150}
-          height={150}
+          width={200}
+          height={200}
           className="mx-auto my-10"
         />
-      </div>
-      <h1 className="flex mx-10 justify-center drop-shadow-2xl text-center text-5xl font-sans font-bold mb-10 text-red-white ">
-        WEEHENA GROUP OF COMPANIES
-      </h1>
+        <h1 className="flex mx-10 justify-center drop-shadow-2xl text-center text-5xl font-sans font-bold mb-10 text-red-white ">
+          WEEHENA GROUP OF COMPANIES
+        </h1>
+      </motion.div>
 
-      <div className="grid mx-auto gap-4 md:grid-cols-2 w-1/2 ">
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 2,
+          scale: { type: "spring", visualDuration: 2, bounce: 0.5 },
+        }}
+        className="grid mx-auto gap-4 grid-cols-1 w-100 "
+      >
         {sites.map((site) => (
           <SiteCard key={site.name} {...site} />
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
