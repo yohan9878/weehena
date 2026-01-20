b402c6476022251ff7fd6004cb6a493dcfaa959a
Yohan Office <yohanoffice78@gmail.com>
2026-01-20 16:40:22 +0530

    Fixed responsiveness on smaller screens

--- app/page.tsx
+++ app/page.tsx
@@ -37,53 +37,113 @@ export default function Home() {
   const isDesktop = useMediaQuery({ query: "(min-width: 768px)" });
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: -75 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 3, ease: "easeOut" }}
-      className=" flex flex-col justify-center md:mx-auto py-10 bg-center bg-black/50 min-w-screen min-h-screen  bg-cover bg-blend-darken"
-      style={{ backgroundImage: "url('/assets/company2.jpg')" }}
-    >
+    <div>
       <motion.div
-        initial={isDesktop? { opacity: 0, scale: 0 }: false}
-        animate={isDesktop? { opacity: 1, scale: 1 } : false }
-        transition={isDesktop? {
-          duration: 2,
-          scale: { type: "spring", visualDuration: 2, bounce: 0.5 },
-        }:undefined}
-        className="w-fit mx-auto"
+        initial={{ opacity: 0, y: -75 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 3, ease: "easeOut" }}
+        className="relative content-center flex flex-col justify-center md:mx-auto py-10 bg-center bg-black/50 min-w-screen min-h-screen  bg-cover bg-blend-darken"
+        style={{ backgroundImage: "url('/assets/company2.jpg')" }}
       >
-        {/* Weehena Logo */}
-        <Image
-          src="/assets/weehenaLogo.png"
-          alt="Company Logo"
-          width={200}
-          height={200}
-          className="w-30 h-30 md:w-40 md:h-40 lg:w-50 lg:h-50 xl:w-50 xl:h-50 mx-auto my-10 drop-shadow-2xl drop-shadow-black bg-white/90 rounded-full"
-        />
-        {/* heading (w-100 h-35) */}
-        <h1 className="flex mx-10 md:mx-auto w-fit sm:w-fit md:w-fit px-2 md:px-4 border-4 border-red-900 py-1 text-white justify-center drop-shadow-xl drop-shadow-black text-center text-xl md:text-3xl lg:text-4xl xl:text-5xl font-sans font-bold mb-10 text-red-white transition ">
-          WEEHENA GROUP OF COMPANIES
-        </h1>
+        <motion.div
+          initial={isDesktop ? { opacity: 0, scale: 0 } : false}
+          animate={isDesktop ? { opacity: 1, scale: 1 } : false}
+          transition={
+            isDesktop
+              ? {
+                  duration: 2,
+                  scale: { type: "spring", visualDuration: 2, bounce: 0.5 },
+                }
+              : undefined
+          }
+          className="w-fit mx-auto"
+        >
+          {/* Weehena Logo */}
+          <Image
+            src="/assets/weehenaLogo.png"
+            alt="Company Logo"
+            width={200}
+            height={200}
+            className="w-30 h-30 md:w-40 md:h-40 lg:w-50 lg:h-50 xl:w-50 xl:h-50 mx-auto my-10 drop-shadow-2xl drop-shadow-black bg-white/90 rounded-full"
+          />
+          {/* heading (w-100 h-35) */}
+          <h1 className="flex mx-10 md:mx-auto w-fit sm:w-fit md:w-fit px-2 md:px-4 border-4 text-white border-red-900/90 py-1  justify-center drop-shadow-xl drop-shadow-black text-center text-xl md:text-3xl lg:text-4xl xl:text-5xl font-sans font-bold mb-10 text-red-white transition ">
+            WEEHENA GROUP OF COMPANIES
+          </h1>
+        </motion.div>
+        <motion.div
+          initial={isDesktop ? { opacity: 0, scale: 0 } : false}
+          animate={isDesktop ? { opacity: 1, scale: 1 } : false}
+          transition={
+            isDesktop
+              ? {
+                  duration: 2,
+                  scale: { type: "spring", visualDuration: 2, bounce: 0.5 },
+                }
+              : undefined //{ duration: 5, delay: 5 }
+          }
+          className="grid xl:justify-stretch xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 mx-auto gap-8 drop-shadow-2xl mt-5"
+        >
+          {sites.map((site) => (
+            <SiteCard key={site.name} {...site} />
+          ))}
+        </motion.div>
+        <div className=" bottom-0 content-center px-10 xl:px-10 md:pt-10 pb-10 mx-auto w-fit mt-10 xl:w-fit   my-auto">
+          <div className="grid mt-10 xl:mt-0 md:grid-cols-5 justify-center gap-10">
+            <div className="w-fit hover:shadow-2xl duration-300 transform hover:scale-110 ease-in-out transition">
+              <Image
+                width={200}
+                height={200}
+                alt="logos"
+                src={"/assets/hotelLogo.png"}
+                className="bg-white size-20 xl:size-30 "
+              />
+              <p className="text-white font-semibold font-sans text-md">company name</p>
+            </div>
+            <div className="w-fit hover:shadow-2xl duration-300 transform hover:scale-110 ease-in-out transition">
+              <Image
+                width={200}
+                height={200}
+                alt="logos"
+                src={"/assets/hotelLogo.png"}
+                className="bg-white size-20 xl:size-30"
+              />
+              <p className="text-white font-semibold font-sans text-md">company name</p>
+            </div>
+            <div className="w-fit hover:shadow-2xl duration-300 transform hover:scale-110 ease-in-out transition">
+              <Image
+                width={200}
+                height={200}
+                alt="logos"
+                src={"/assets/hotelLogo.png"}
+                className="bg-white size-20 xl:size-30"
+                //bg-white size-20 xl:size-30 hover:shadow-2xl  hover:shadow-red-100 duration-300 transform hover:scale-110 ease-in-out transition
+              />
+              <p className="text-white font-semibold font-sans text-md">company name</p>
+            </div>
+            <div className="w-fit hover:shadow-2xl duration-300 transform hover:scale-110 ease-in-out transition">
+              <Image
+                width={200}
+                height={200}
+                alt="logos"
+                src={"/assets/hotelLogo.png"}
+                className="bg-white size-20 xl:size-30 "
+              />
+              <p className="text-white font-semibold font-sans text-md">company name</p>
+            </div>
+            <div className="w-fit hover:shadow-2xl duration-300 transform hover:scale-110 ease-in-out transition">
+              <Image
+                width={200}
+                height={200}
+                alt="logos"
+                src={"/assets/hotelLogo.png"}
+                className="bg-white size-20 xl:size-30 "
+              />
+              <p className="text-white font-semibold font-sans text-md">company name</p>
+            </div>
+          </div>
+        </div>
       </motion.div>
-
-      <motion.div
-        initial={isDesktop ? { opacity: 0, scale: 0 } : false}
-        animate={isDesktop ? { opacity: 1, scale: 1 } : false}
-        transition={
-          isDesktop
-            ? {
-                duration: 2,
-                scale: { type: "spring", visualDuration: 2, bounce: 0.5 },
-              }
-            : undefined //{ duration: 5, delay: 5 }
-        }
-        className="grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 mx-auto gap-4 drop-shadow-2xl mt-5"
-      >
-        {sites.map((site) => (
-          <SiteCard key={site.name} {...site} />
-        ))}
-      </motion.div>
-    </motion.div>
+    </div>
   );
 }
--- components/SiteCard.tsx
+++ components/SiteCard.tsx
@@ -8,13 +8,13 @@ type Props = {
 
 export default function SiteCard({ name, url, img }: Props) {
   return (
-    <div className="flex mx-auto justify-center flex-col lg:flex-col-2 w-fit h-20 md:w-100 lg:w-100 lg:h-25 xl:w-100 xl:h-25 mb-2 rounded-full bg-gray-100/70 hover:shadow-2xl  hover:bg-white hover:shadow-red-100 transition">
+    <div className="flex mx-auto justify-center flex-col lg:flex-col-2 w-fit h-20 md:w-100 lg:w-100 lg:h-25 xl:w-100 xl:h-25 mb-2 rounded-full hover:shadow-2xl bg-red-100/80 hover:bg-white hover:shadow-red-100 duration-300 transform hover:scale-110 ease-in-out transition">
       <a href={url} target="_blank" rel="noopener noreferrer">
         <div className="flex ml-5 xl:ml-5 mx-auto gap-2 sm:gap-0 lg:w-100 xl:w-100 h-25">
           <Image
             src={img}
-            width={80}
-            height={60}
+            width={100}
+            height={100}
             alt="logos"
             className="my-auto rounded-xl w-10 h-10 lg:w-15 lg:h-15 xl:w-15 xl:h-15 "
           />
